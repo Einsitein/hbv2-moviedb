362da23d370ce8fb7f042b95aa581bb7a0955fba
Gylfi Styrmisson <gylfisstyrmisson@gmail.com>
2025-03-09 20:04:19 +0000

    2 aukastafir á einkunn og sækir current user

--- app/src/main/java/is/hbv601g/movieapp/MovieDetailsActivity.kt
+++ app/src/main/java/is/hbv601g/movieapp/MovieDetailsActivity.kt
@@ -11,6 +11,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
 import com.bumptech.glide.Glide
 import `is`.hbv601g.movieapp.model.ReviewItem
+import `is`.hbv601g.movieapp.model.UserItem
 import `is`.hbv601g.movieapp.network.RetrofitInstance
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -19,6 +20,7 @@ import kotlin.coroutines.cancellation.CancellationException
 
 
 class MovieDetailsActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,20 +35,21 @@ class MovieDetailsActivity : AppCompatActivity() {
 
         // Retrieve the passed movie ID
         val movieId = intent.getIntExtra("MOVIE_ID", -1)
-        val userId = intent.getIntExtra("USER_ID", -1)
 
 
         if (movieId != -1) {
             lifecycleScope.launch {
                 try {
                     val movie = RetrofitInstance.movieApiService.getMovieById(movieId)
-                    val averageRating = RetrofitInstance.reviewApiService.getMovieRatingById(movieId)
+                    val averageRating =
+                        RetrofitInstance.reviewApiService.getMovieRatingById(movieId)
+
 
 
                     if (movie.isSuccessful && averageRating.isSuccessful) {
                         movieName.text = movie.body()?.name ?: "Unknown"
                         movieYear.text = movie.body()?.year?.toString() ?: "Unknown"
-                        movieRating.text = "Rating: " + averageRating.body().toString()
+                        movieRating.text = "Rating: " + String.format("%.2f", averageRating.body())
 
                         // Load movie poster from URL using Glide
                         Glide.with(this@MovieDetailsActivity)
@@ -72,48 +75,69 @@ class MovieDetailsActivity : AppCompatActivity() {
                     return@setOnClickListener
                 }
 
-                val review = ReviewItem(
-                    id = "${userId}-$movieId",
-                    userId = userId.toLong(),
-                    movieId = movieId.toLong(),
-                    rating = userRating,
-                    movieReview = ""
-                )
 
-                lifecycleScope.launch {
-                    Log.d("MovieApp", userRating.toString())
-                    try {
-                        val response = withContext(Dispatchers.IO) {
-                            RetrofitInstance.reviewApiService.submitReview(review)
-                        }
+                    lifecycleScope.launch {
 
-                        withContext(Dispatchers.Main) {
-                            if (response.isSuccessful) {
-                                Toast.makeText(this@MovieDetailsActivity, "Your rating: $userRating", Toast.LENGTH_SHORT).show()
-                                val updatedRatingResponse = withContext(Dispatchers.IO) {
-                                    RetrofitInstance.reviewApiService.getMovieRatingById(movieId)
+                        try {
+                            val token = DBHelper(applicationContext).getLatestToken()
+                            val response = token?.let { it1 -> RetrofitInstance.userApiService.getMe(it1) }
+                            val userId = response?.body()?.id
+
+
+                            if(userId != null) {
+                                val review = ReviewItem(
+                                    id = "${userId}-$movieId",
+                                    userId = userId.toLong(),
+                                    movieId = movieId.toLong(),
+                                    rating = userRating,
+                                    movieReview = ""
+                                )
+
+                                val response = withContext(Dispatchers.IO) {
+                                    RetrofitInstance.reviewApiService.submitReview(review)
                                 }
-
-                                // Update the UI with the new rating
                                 withContext(Dispatchers.Main) {
-                                    if (updatedRatingResponse.isSuccessful) {
-                                        val newAverageRating = updatedRatingResponse.body()
-                                        movieRating.text = "Rating: ${newAverageRating?.toString() ?: "N/A"}"
+                                    if (response.isSuccessful) {
+                                        Toast.makeText(
+                                            this@MovieDetailsActivity,
+                                            "Your rating: $userRating",
+                                            Toast.LENGTH_SHORT
+                                        ).show()
+                                        val updatedRatingResponse = withContext(Dispatchers.IO) {
+                                            RetrofitInstance.reviewApiService.getMovieRatingById(movieId)
+                                        }
+
+                                        // Update the UI with the new rating
+                                        withContext(Dispatchers.Main) {
+                                            if (updatedRatingResponse.isSuccessful) {
+                                                val newAverageRating = updatedRatingResponse.body()
+                                                movieRating.text =
+                                                    "Rating: " + String.format("%.2f", newAverageRating)
+                                            } else {
+                                                Toast.makeText(
+                                                    this@MovieDetailsActivity,
+                                                    "Failed to fetch updated rating!",
+                                                    Toast.LENGTH_SHORT
+                                                ).show()
+                                            }
+                                        }
                                     } else {
                                         Toast.makeText(
                                             this@MovieDetailsActivity,
-                                            "Failed to fetch updated rating!",
+                                            "Failed to submit rating!",
                                             Toast.LENGTH_SHORT
                                         ).show()
                                     }
                                 }
-                            } else {
-                                Toast.makeText(this@MovieDetailsActivity, "Failed to submit rating!", Toast.LENGTH_SHORT).show()
                             }
-                        }
-                    } catch (e: Exception) {
-                        withContext(Dispatchers.Main) {
-                            Toast.makeText(this@MovieDetailsActivity, "Network error: ${e.message}", Toast.LENGTH_SHORT).show()
+                        } catch (e: Exception) {
+                            withContext(Dispatchers.Main) {
+                                Toast.makeText(
+                                    this@MovieDetailsActivity,
+                                    "Network error: ${e.message}",
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                            }
                         }
                     }
                 }
@@ -122,4 +146,4 @@ class MovieDetailsActivity : AppCompatActivity() {
 
         }
     }
-}
+
